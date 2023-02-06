9c550638235ee0906f3679964bcffe4fd0b1a02c
Julia Alberici <julia.jpa0@gmail.com>
2023-02-06 20:55:11 -0300

    add user auth validation to render nav option

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,19 +1,24 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
+import { map, Observable } from 'rxjs';
 import IUser from '../models/user.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  private usersCollection: AngularFirestoreCollection<IUser>
+  private usersCollection: AngularFirestoreCollection<IUser>;
+  public isAuthenticated$: Observable<boolean>;
 
   constructor(
     private auth: AngularFireAuth,
     private db: AngularFirestore
   ) {
     this.usersCollection = db.collection("users");
+    this.isAuthenticated$ = auth.user.pipe(
+      map(user => !!user)
+    );
    }
 
   public async createUser(userData: IUser){
