496443352b8c2517eaaf0cfd8e67ce3f5b7a2d61
Julia Alberici <julia.jpa0@gmail.com>
2023-02-06 21:20:25 -0300

    add login

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+
 
 @Component({
   selector: 'app-login',
@@ -11,13 +13,36 @@ export class LoginComponent implements OnInit {
     password: ''
   }
 
-  constructor() { }
+  inSubmission = false;
+
+  constructor(private auth: AngularFireAuth) { }
 
   ngOnInit(): void {
   }
 
-  login(){
-    console.log('user logged in')
+  showAlert = false
+  alertMsg = 'Please wait! Your account is being logged in.'
+  alertColor = 'blue'
+
+  async login(){
+    this.showAlert = true
+    this.alertMsg = 'Please wait! You\'re being logged in.'
+    this.alertColor = 'blue'
+    this.inSubmission = true;
+
+    try {
+      await this.auth.signInWithEmailAndPassword(
+        this.credentials.email, this.credentials.password
+        )
+      }catch {
+      this.showAlert = true
+      this.alertMsg = 'An unexpected error occurred. Please try again later.'
+      this.alertColor = 'red'
+      this.inSubmission = false;
+      return;
+    }
+    this.alertMsg = "You're logged in."
+    this.alertColor = 'green'
   }
 
 }
